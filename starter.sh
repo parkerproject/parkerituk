16df37475f687051ee4ad285b5fbc0bdf3338a7f
Parker <parkerproject@gmail.com>
2015-04-19 18:18:15 -0400

    updates

--- starter.sh
+++ starter.sh
@@ -3,6 +3,6 @@
 if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
   export PATH=/usr/local/bin:$PATH
-  export NODE_ENV=production
+  #export NODE_ENV=production
   cd /var/www/parkerituk && forever --spinSleepTime 10000 start index.js >> forever.log 2>&1
 fi
